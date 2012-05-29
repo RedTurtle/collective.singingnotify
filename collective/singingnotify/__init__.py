c38d6f0d5ed4e3949735de0099a3e79a4fa57825
micecchi <micecchi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-05-29 12:08:45 +0000

    initial import

---
+++ collective/singingnotify/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from zope.i18nmessageid import MessageFactory
+MessageFactory = MessageFactory('collective.singingnotify')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+setup(name='collective.singingnotify',
+      version=version,
+      description="A plugin for Singing&Dancing that notify unsubscriptions",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='singing dancing newsletter notification',
+      author='RedTurtle Techonology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
